2e71fb56b19d3f45adc713e48a94bff3985d6719
kingsmooth95 <srh0701@gmail.com>
2019-04-23 13:05:37 +0800

    Code style

--- Zebro/index.ios.js
+++ Zebro/index.ios.js
@@ -62,7 +62,7 @@ var Zebro = React.createClass({
         nextCard={this.createdDeck}
         {...route.data}/>;
     case 'review':
-      return <Review {...route.data} />;
+      return <Review quit={this.goHome} {...route.data} />;
     default:
       console.error('Encountered unexpected route: ' + route.name);
     }
@@ -85,7 +85,6 @@ var Zebro = React.createClass({
 var styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#F5FCFF',
     marginTop: 30
   }
 });
--- Zebro/src/styles/fonts.js
+++ Zebro/src/styles/fonts.js
@@ -2,12 +2,18 @@ import { StyleSheet } from 'react-native';
 
 var fonts = StyleSheet.create({
   normal: {
-    fontSize: 16,
-    fontFamily: 'Avenir'
+    fontSize: 24,
+    fontFamily: 'Avenir Medium'
+  },
+
+  alternate: {
+    fontSize: 50,
+    fontFamily: 'Avenir Heavy',
+    color: '#FFFFFF'
   },
 
   big: {
-    fontSize: 24,
+    fontSize: 32,
     alignSelf: 'center',
     fontFamily: 'Avenir Medium'
   }
