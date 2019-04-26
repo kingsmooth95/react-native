1f50b6a0bcce28cfb67ab5aac5af43842ee63e93
kingsmooth95 <srh0701@gmail.com>
2019-04-25 23:28:07 -0400

    Commit save.sh

--- WeatherProject/WeatherProject.js
+++ WeatherProject/WeatherProject.js
@@ -7,51 +7,110 @@ var {
   Image
 } = React;
 
+var Forecast = require('./Forecast');
 
 var WeatherProject = React.createClass({
-  getInitialState() {
-    return ({
-      zip: ''
-    });
+  getInitialState: function() {
+    return {
+      zip: '',
+      forecast: null
+    };
   },
-  _handleTextChange(event) {
-    console.log(event.nativeEvent.text);
-    this.setState({
-      zip: event.nativeEvent.text
-    });
+
+  _handleTextChange: function(event) {
+    var zip = event.nativeEvent.text;
+    this.setState({zip: zip});
+    fetch('http://api.openweathermap.org/data/2.5/weather?q='
+      + zip + '&units=imperial')
+      .then((response) => response.json())
+      .then((responseJSON) => {
+        this.setState({
+          forecast: {
+            main: responseJSON.weather[0].main,
+            description: responseJSON.weather[0].description,
+            temp: responseJSON.main.temp
+          }
+        });
+      })
+      .catch((error) => {
+        console.warn(error);
+      });
   },
-  render() {
+
+  render: function() {
+    var content = null;
+    if (this.state.forecast !== null) {
+      content = <Forecast 
+                  main={this.state.forecast.main}
+                  description={this.state.forecast.description}
+                  temp={this.state.forecast.temp}/>;
+    }
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          You input {this.state.zip}.
-        </Text>
-        <TextInput
-              style={styles.input}
-              returnKeyType='go'
-              onSubmitEditing={this._handleTextChange}/>
+        <Image source={require('image!flowers')}
+               resizeMode='cover'
+               style={styles.backdrop}>
+          <View style={styles.overlay}>
+           <View style={styles.row}>
+             <Text style={styles.mainText}>
+               Current weather for 
+             </Text>
+             <View style={styles.zipContainer}>
+               <TextInput
+                 style={[styles.zipCode, styles.mainText]}
+                 onSubmitEditing={this._handleTextChange}/>
+             </View>
+           </View>
+           {content}
+         </View>
+        </Image>
       </View>
     );
   }
 });
 
+var baseFontSize = 16;
+
 var styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    paddingTop: 30
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+  backdrop: {
+    flex: 1,
+    flexDirection: 'column'
   },
-  input: {
-    fontSize: 20,
-    borderWidth: 2,
-    height: 40
-    }
+  overlay: {
+    paddingTop: 5,
+    backgroundColor: '#000000',
+    opacity: 0.5,
+    flexDirection: 'column',
+    alignItems: 'center'
+  },
+  row: {
+    flex: 1,
+    flexDirection: 'row',
+    flexWrap: 'nowrap',
+    alignItems: 'flex-start',
+    padding: 30
+  },
+  zipContainer: {
+    flex: 1,
+    borderBottomColor: '#DDDDDD',
+    borderBottomWidth: 1,
+    marginLeft: 5,
+    marginTop: 3
+  },
+  zipCode: {
+    width: 50,
+    height: baseFontSize,
+  },
+  mainText: {
+    flex: 1,
+    fontSize: baseFontSize,
+    color: '#FFFFFF'
+  }
 });
 
 module.exports = WeatherProject;
